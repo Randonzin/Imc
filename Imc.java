29aed2e122b666ee643c5579b5b49099fbe1cdcd
Randonzin <kogman32@gmail.com>
2026-05-07 22:21:27 -0300

    uptade imc.java

---
+++ Imc.java
@@ -0,0 +1,10 @@
+public class Imc{
+    public static void main(String[] args){
+        double peso = 0;
+        double altura = 0;
+
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.println("Qual é o seu peso? ");
+        peso = leitura.nextDouble();
+        System.out.println("Qual é a sua altura? ");
